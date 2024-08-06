83bdd27ea80f38a839afa9cc1103c9e3d7dc88de
SUA <sebastiansua90@gmail.com>
2024-08-06 17:00:56 -0500

    Proyect Init, Set the most of the Dependencies

---
+++ BackEnd/src/main/java/com/esomos/videogestion/VideogestionApplication.java
@@ -0,0 +1,13 @@
+package com.esomos.videogestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogestionApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/esomos/videogestion/VideogestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esomos.videogestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
